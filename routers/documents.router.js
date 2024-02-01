360acebd1170c0e1713eaeec8cd175db61c0b79c
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-02-02 02:45:24 +0900

    edited app.js/index.js

--- routers/documents.router.js
+++ routers/documents.router.js
@@ -1,7 +1,30 @@
 // 이력서 관련
 
 import express from "express";
+import { prisma } from "../models/index.js";
 
 const router = express.Router();
+
+// 이력서 생성 | POST
+
+router.post("/documents", async (req, res, next) => {
+  return res.status(201).json({ data: post });
+});
+
+// 이력서 수정 | PUT
+
+router.post("/documents/:documentId", async (req, res, next) => {
+  return res.status(200).json({ data: "이력서가 수정되었습니다." });
+});
+
+// 이력서 삭제 | DELETE
+
+router.delete("/documents/:documentId", async (req, res, next) => {
+  return res.status(200).json({ data: "게시글이 삭제되었습니다." });
+});
+
+// 이력서 목록 조회 | READ
+
+// 이력서 상세 조회 | READ
 
 export default router;
--- routers/users.router.js
+++ routers/users.router.js
@@ -1,6 +1,7 @@
 // 사용자 관련
 
 import express from "express";
+import { prisma } from "../models/index.js";
 
 const router = express.Router();
 
