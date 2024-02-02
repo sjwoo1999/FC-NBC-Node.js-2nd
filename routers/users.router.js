4979a7e04710d7a345f4aa4cc9663ff036870e06
_woo_s.j <sjwoo1999@korea.ac.kr>
2024-02-02 11:39:34 +0900

    modified routers/users.router.js : login API

--- routers/users.router.js
+++ routers/users.router.js
@@ -3,6 +3,7 @@
 import express from "express";
 import { prisma } from "../utils/prisma/index.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -32,6 +33,30 @@ router.post("/sign-up", async (req, res, next) => {
   });
 
   return res.status(201).json({ message: "회원가입이 완료되었습니다." });
+});
+
+/* 로그인 API */
+
+router.post("/sign-in", async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await prisma.users.findFirst({ where: { email } });
+
+  if (!user)
+    return res.status(401).json({ message: "존재하지 않는 이메일입니다." });
+  else if (!(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ message: "비밀번호가 일치하지 않습니다." });
+
+  // 로그인에 성공하면, 사용자의 userId를 바탕으로 토큰을 생성합니다.
+  const token = jwt.sign(
+    {
+      userId: user.userId,
+    },
+    "custom-secret-key",
+  );
+
+  // authoization 쿠키에 Bearer 토큰 형식으로 JWT를 저장합니다.
+  res.cookie("authorization", `Bearer ${token}`);
+  return res.status(200).json({ message: "로그인 성공" });
 });
 
 export default router;
